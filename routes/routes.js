6e813bedbbed7e9814327b7a5f518510ab6098fb
Logan Munsee <lmmunsee@students.fullsail.edu>
2025-08-18 11:11:14 -0700

    removed unnessary route

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,5 @@ const router = express.Router();
 const contentController = require("../controllers/content_controller");
 
 router.get("/", contentController.getAllContent);
-router.post("/", contentController.createContent);
 // more routes may be added here
 module.exports = router;
