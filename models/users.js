be6375ec8f1ef31b8c29269c742706836017a3ef
MunseeLogan-FS <lmmunsee@student.fullsail.edu>
2025-09-03 01:28:35 -0400

    one small change

--- models/users.js
+++ models/users.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  spotifyId: { type: String, required: true, unique: true },
-  accessToken: String,
-  refreshToken: String,
-  tokenExpiresAt: Date,
-});
+const userSchema = new mongoose.Schema(
+  {
+    spotifyId: { type: String, required: true, unique: true },
+    accessToken: String,
+    refreshToken: String,
+    tokenExpiresAt: Date,
+  },
+  { collection: "spotify" }
+);
 
 export default mongoose.model("User", userSchema);
