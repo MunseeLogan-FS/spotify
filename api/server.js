8fa8ebb65ca015563582ef54c8aa91e08532e78f
MunseeLogan-FS <lmmunsee@student.fullsail.edu>
2025-09-04 01:54:01 -0400

    test

--- api/server.js
+++ api/server.js
@@ -7,8 +7,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    // origin: "http://127.0.0.1:5173",
-    origin: process.env.FRONTEND_URI,
+    origin: "https://myspotapp.netlify.app",
     credentials: true,
   })
 );
